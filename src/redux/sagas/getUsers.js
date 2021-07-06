154788f846dbd95d5cd6ba0cded7e06a9bf11b72
Lusine-lab <petrosyan.lusine12@gmail.com>
2021-07-06 21:38:43 +0400

    fix saga

--- src/redux/sagas/getUsers.js
+++ src/redux/sagas/getUsers.js
@@ -15,9 +15,16 @@ export function* getUserSaga(action) {
 export function* createUserSaga(action) {
   try {
     console.log("tTTTTTTTTTTT " + JSON.stringify(action));
-    const response = yield call(addUser(action.user));
+   /* const response =*/ yield call(addUser, action.user);
+    //
+    //
+    // const data = response.data;
+    // yield put(setUsers(data));
+
+    const response = yield call(getUserList);
     const data = response.data;
     yield put(setUsers(data));
+
   } catch (error) {
     console.log("getUserSaga", error);
   }
